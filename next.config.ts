84382c4002589b8a39c5786e165ee1f5b0f08b6c
YheChen <yanzhenchen123@gmail.com>
2025-05-24 23:37:21 -0400

    save point

--- next.config.ts
+++ next.config.ts
@@ -1,28 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async headers() {
-    return [
-      {
-        // apply these headers to all routes in your app
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: [
-              "default-src 'self'",
-              "script-src 'self'",
-              "style-src 'self' 'unsafe-inline'",
-              "img-src 'self' data:",
-              "font-src 'self'",
-              "object-src 'none'",
-              "frame-ancestors 'none'",
-            ].join("; "),
-          },
-        ],
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
